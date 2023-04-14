5790ba464c251df31e68442abd8d86822ccff4dc
hdthinh1012 <hdthinh01@gmail.com>
2023-04-14 17:55:57 +0700

    Update CMD script to match solidity name

--- tools/qltest.sh
+++ tools/qltest.sh
@@ -4,10 +4,7 @@ set -eu
 
 exec "${CODEQL_DIST}/codeql" database index-files \
     --prune="**/*.testproj" \
-    --include-extension=.rb \
-    --include-extension=.erb \
-    --include-extension=.gemspec \
-    --include="**/Gemfile" \
+    --include-extension=.sol \
     --size-limit=5m \
     --language=solidity \
     --working-dir=.\
